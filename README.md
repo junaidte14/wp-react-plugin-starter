56614def2449cb250cf886e1ba162a353d66afbf
Junaid Hassan <junaid.hassan@iep.local>
2020-10-22 01:38:59 +0500

    opts, and settings page

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 A starter WordPress plugin which shows how to use React in a WordPress plugin.
+
+### Setup
+
+To start using all the tools that come with `wp-react-plugin-starter`  you need to install the necessary Node.js dependencies :
+
+```
+npm install
+```
+
+### Available CLI commands
+
+`wp-react-plugin-starter` comes packed with CLI commands :
+
+- `npm run dev` : webpack watches src directory for changes and output the bundled file and assests inside the /dist folder.
+- `npm run bundle` : generates a .zip archive for distribution, excluding development and system files.
+
+Now you're ready to go!
+
+Good luck!
--- wp-react-plugin-starter.php
+++ wp-react-plugin-starter.php
@@ -17,3 +17,48 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
+
+// Action to add menu in settings page.
+add_action( 'admin_menu', 'codowpreact_admin_menu' );
+
+/**
+ * Function to add new menu in settings
+ *
+ * @since   1.0.0
+ */
+function codowpreact_admin_menu() {
+	add_options_page(
+		__( 'CODO WP REACT', 'codowpreact' ),
+		__( 'CODO WP REACT', 'codowpreact' ),
+		'manage_options',
+		'codowpreact',
+		'display_codowpreact_admin_page'
+	);
+}
+
+/**
+ * Callback function of Settings Page
+ *
+ * @since    1.0.0
+ */
+function display_codowpreact_admin_page() {
+	?>
+	<h1>Welcome to codowpreact! Following is the rendered REACT app.</h1><br />
+    <div id="root"></div>
+	<?php
+}
+
+// Action to add scripts to admin side.
+add_action( 'admin_enqueue_scripts', 'codowpreact_enqueue_admin_scripts' );
+
+/**
+ * Function to add the scripts and styles to admin page.
+ *
+ * @since 1.0.0
+ */
+function codowpreact_enqueue_admin_scripts() {
+	wp_enqueue_script( 'codowpreact-script', plugin_dir_url( __FILE__ ) . '/dist/bundle.js', array(
+		'jquery',
+		'wp-element'
+	), '1.0.0' );
+}
